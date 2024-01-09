c5330c297f959e77534bf92d0f0425c59f572072
Kim-Ji-Yeong <yeong6415@daum.net>
2024-01-10 03:11:28 +0900

    section10

---
+++ section10/src/chpater1.ts
@@ -0,0 +1,56 @@
+/**
+ * Partial<T>
+ * -> 부분적인, 일부분의
+ * -> 특정 객체 타입의 모든 프로퍼티를 선택적 프로퍼티 바꿔주는 타입
+ */
+
+interface Post {
+  title: string;
+  tags: string[];
+  content: string;
+  thumbnailURL?: string;
+}
+
+type Partial<T> = {
+  [key in keyof T]?: T[key];
+};
+
+const draft: Partial<Post> = {
+  title: "TIETLE",
+  content: "CONTENT",
+};
+
+/**
+ * Required<T>
+ * -> 필수의, 필수적인
+ * -> 특정 객체 타입의 모든 프로퍼티를 필수 프로퍼티로 바꿔주는 타입
+ */
+
+type Required<T> = {
+  [key in keyof T]-?: T[key];
+};
+
+const withTunmbnailPost: Required<Post> = {
+  title: "TITLE",
+  tags: ["ts"],
+  content: "CONTENT",
+  thumbnailURL: "https://...",
+};
+
+/**
+ * Readonly<T>
+ * -> 일기전용 수정불가
+ * -> 특정 객체 타입에서 모든 프로퍼티를 읽기 전용 프로퍼티로 만들어주는 타입
+ */
+
+type Readonly<T> = {
+  readonly [key in keyof T]: T[key];
+};
+
+const readonlyPost: Readonly<Post> = {
+  title: "TITLE",
+  tags: [],
+  content: "",
+};
+
+// readonlyPost.content = "";
